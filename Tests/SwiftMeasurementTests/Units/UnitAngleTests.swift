ff5f950fdee718686ecd244b70bcf34eac801421
Ken Tominaga <ken.for.develop@gmail.com>
2025-05-01 14:10:08 -0700

    Create UnitAngleTests

---
+++ Tests/SwiftMeasurementTests/Units/UnitAngleTests.swift
@@ -0,0 +1,14 @@
+import Foundation
+import Testing
+@testable import SwiftMeasurement
+
+@Test func testUnitAngle() {
+    let value = 1.0
+
+    #expect(value.degrees == Measurement<UnitAngle>(value: value, unit: .degrees))
+    #expect(value.arcMinutes == Measurement<UnitAngle>(value: value, unit: .arcMinutes))
+    #expect(value.arcSeconds == Measurement<UnitAngle>(value: value, unit: .arcSeconds))
+    #expect(value.radians == Measurement<UnitAngle>(value: value, unit: .radians))
+    #expect(value.gradians == Measurement<UnitAngle>(value: value, unit: .gradians))
+    #expect(value.revolutions == Measurement<UnitAngle>(value: value, unit: .revolutions))
+}
